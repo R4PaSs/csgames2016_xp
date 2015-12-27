820d16532099e61d1599104868f00aff6f76f0a2
Philippe Pepos Petitclerc <ppeposp@gmail.com>
2015-12-26 19:15:48 -0500

    let django take care of id field

--- src/xtreme_programming/comp_auth/models.py
+++ src/xtreme_programming/comp_auth/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Team(models.Model):
-    id = models.IntegerField(primary_key=True)
+    # id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=128)
     token = models.SlugField()
     user = models.OneToOneField(User, null=True)
